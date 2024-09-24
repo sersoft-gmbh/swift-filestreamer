87a5cdf57465d3b5ee045aa02ddef2f0e09e4410
Florian Friedrich <ffried@me.com>
2024-09-24 08:07:21 +0200

    Add Swift 6.0 support

--- Sources/FileStreamer/FileStream.swift
+++ Sources/FileStreamer/FileStream.swift
@@ -1,22 +1,40 @@
-import Dispatch
-import SystemPackage
+fileprivate import Dispatch
+public import SystemPackage
 
 /// An async sequence that continously streams the generic `Value` type from a given file.
 public struct FileStream<Value>: AsyncSequence {
     public typealias Element = AsyncIterator.Element
-    
+    public typealias Failure = AsyncIterator.Failure
+
     /// Defines how failures should be handled.
     public struct FailureBehavior: Sendable {
+#if swift(>=6.0)
+        private let handler: @Sendable (any Error) throws(Failure) -> ()
+#else
         private let handler: @Sendable (any Error) throws -> ()
+#endif
 
-        func handleError(_ error: some Error) throws {
+#if swift(>=6.0)
+        internal func handleError(_ error: any Error) throws(Failure) {
             try handler(error)
         }
+#else
+        internal func handleError(_ error: any Error) throws {
+            try handler(error)
+        }
+#endif
 
+#if swift(>=6.0)
+        /// The error is handled by the given closure. Throwing from the closure will terminate the sequence.
+        public static func custom(_ handler: @escaping @Sendable (any Error) throws(Failure) -> ()) -> Self {
+            .init(handler: handler)
+        }
+#else
         /// The error is handled by the given closure. Throwing from the closure will terminate the sequence.
         public static func custom(_ handler: @escaping @Sendable (any Error) throws -> ()) -> Self {
             .init(handler: handler)
         }
+#endif
 
         /// Any error is thrown from the sequence (terminating it).
         public static var `throw`: Self { .init { throw $0 } }
@@ -28,11 +46,12 @@ public struct FileStream<Value>: AsyncSequence {
     }
 
     @usableFromInline
-    typealias Stream = AsyncThrowingStream<Element, any Error>
+    typealias Stream = AsyncThrowingStream<Element, Failure>
 
     @frozen
     public struct AsyncIterator: AsyncIteratorProtocol {
         public typealias Element = Value
+        public typealias Failure = any Error
 
         @usableFromInline
         var _iterator: Stream.AsyncIterator
@@ -46,6 +65,14 @@ public struct FileStream<Value>: AsyncSequence {
         public mutating func next() async throws -> Element? {
             try await _iterator.next()
         }
+
+#if swift(>=6.0)
+        @inlinable
+        @available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *)
+        public mutating func next(isolation actor: isolated (any Actor)?) async throws(Failure) -> Value? {
+            try await _iterator.next(isolation: `actor`)
+        }
+#endif
     }
 
     @usableFromInline
@@ -70,30 +97,40 @@ extension FileStream {
                                            using cont: Stream.Continuation,
                                            handlingFailuresWith failureBehavior: FailureBehavior) {
         let source = _inactiveSource(from: fileDescriptor) {
-            switch $0 {
-            case .success(let values):
-                for elem in values {
-                    if case .terminated = cont.yield(elem) {
-                        break
-                    }
-                }
-            case .failure(let error):
-                do {
-                    try failureBehavior.handleError(error)
-                } catch {
-                    cont.finish(throwing: error)
-                }
+            cont.yield($0.value)
+        } onFailure: {
+            do {
+                try failureBehavior.handleError($0)
+            } catch {
+                cont.finish(throwing: error)
             }
         }
-        cont.onTermination = { _ in _terminateSource(source) }
+        cont.onTermination = { _ in source.cancel() }
         source.activate()
     }
 }
 
+fileprivate struct SendableDispatchSource: @unchecked Sendable {
+    let source: any DispatchSourceRead
+
+    func activate() {
+        source.activate()
+    }
+
+    func cancel() {
+        source.cancel()
+    }
+}
+
+fileprivate struct UncheckedSendable<T>: @unchecked Sendable {
+    let value: T
+}
+
 extension FileStream {
     private static func _inactiveSource(from fileDesc: FileDescriptor,
-                                        informing callback: @escaping (Result<Array<Element>, any Error>) -> ()) -> any DispatchSourceRead {
-        let workerQueue = DispatchQueue(label: "de.sersoft.filestreamer.filestream.worker")
+                                        onElement elementCallback: @escaping (UncheckedSendable<Element>) -> (),
+                                        onFailure failureCallback: @escaping (any Error) -> ()) -> SendableDispatchSource {
+        let workerQueue = DispatchQueue(label: "de.sersoft.filestreamer.filestream.gcd.worker")
         let source = DispatchSource.makeReadSource(fileDescriptor: fileDesc.rawValue, queue: workerQueue)
         let rawSize = MemoryLayout<Value>.size
         let rawSize64 = UInt64(rawSize)
@@ -102,11 +139,13 @@ extension FileStream {
             do {
                 remainingData += .init(source.data)
                 guard case let capacity = remainingData / rawSize64, capacity > 0 else { return }
-                let buffer = UnsafeMutableBufferPointer<Value>.allocate(capacity: .init(capacity))
+                let buffer = UnsafeMutableBufferPointer<Element>.allocate(capacity: .init(capacity))
                 defer { buffer.deallocate() }
                 let bytesRead = try fileDesc.read(into: UnsafeMutableRawBufferPointer(buffer))
                 if case let noOfValues = bytesRead / rawSize, noOfValues > 0 {
-                    callback(.success(Array(buffer.prefix(noOfValues))))
+                    for value in buffer.prefix(noOfValues) {
+                        elementCallback(.init(value: value))
+                    }
                 }
                 let leftOverBytes = bytesRead % rawSize
                 remainingData -= .init(bytesRead - leftOverBytes)
@@ -116,17 +155,13 @@ extension FileStream {
                     } catch {
                         // If we failed to seek, we need to drop the left-over bytes.
                         remainingData -= .init(leftOverBytes)
-                        callback(.failure(error))
+                        throw error // Re-throw to land it in the failureCallback below
                     }
                 }
             } catch {
-                callback(.failure(error))
+                failureCallback(error)
             }
         }
-        return source
-    }
-
-    private static func _terminateSource(_ source: some DispatchSourceRead) {
-        source.cancel()
+        return .init(source: source)
     }
 }
